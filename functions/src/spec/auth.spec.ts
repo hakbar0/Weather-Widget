2a2f0f0c6e5a92d3e15f0ec5e4cb19f35ee450c0
hakbar0 <haseeb2000@hotmail.com>
2021-11-06 20:20:44 +0000

    created new test to check for fail auth

--- functions/src/spec/auth.spec.ts
+++ functions/src/spec/auth.spec.ts
@@ -3,19 +3,31 @@ import axios from "axios";
 
 import { fbAuth } from "../../config/config";
 
-const getidToken = async () => {
-  const { email, password, returnSecureToken, APIKEY, gURL } = fbAuth;
-  const res = await axios({
-    method: "POST",
-    url: `${gURL}${APIKEY}`,
-    data: { email, password, returnSecureToken },
-  });
-  return res;
+const getidToken = async (flag: boolean = true) => {
+  try {
+    const { email, password, returnSecureToken, gURL } = fbAuth;
+    let { APIKEY } = fbAuth;
+
+    if (!flag) APIKEY = "NOTVALIDAPIKEY";
+    const res = await axios({
+      method: "POST",
+      url: `${gURL}${APIKEY}`,
+      data: { email, password, returnSecureToken },
+    });
+    return res;
+  } catch (err) {
+    //Throw 400 error
+    return { status: 400 };
+  }
 };
 
 describe("Able to authetnicate and get idToken", () => {
   it("Response recieved 200", async () => {
     const res = await getidToken();
     expect(res.status).toBe(200);
+  });
+  it("Response recieved should not be 200", async () => {
+    const res = await getidToken(false);
+    expect(res.status).toBe(400);
   });
 });
